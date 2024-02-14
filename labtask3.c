7005f2308f35c6b794165d5e66463b9a24c41d39
Akhil-Baki <157685874+Akhil-Baki@users.noreply.github.com>
2024-02-14 22:46:03 +0530

    Update labtask3.c

--- labtask3.c
+++ labtask3.c
@@ -42,3 +42,41 @@ int main() {
 
     return 0;
 }
+//linear search 
+
+#include <stdio.h>
+
+int linear_search(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1; // Target not found
+}
+
+int main() {
+    int n, i, target;
+    printf("enter the size of array:");
+    scanf("%d", &n);
+
+    int a[n]; // Declare a with size n
+
+    for (i = 0; i < n; i++) {
+        printf("enter element in the array of index %d:", i);
+        scanf("%d", &a[i]);
+    }
+
+    printf("enter the target:");
+    scanf("%d", &target);
+
+    int index = linear_search(a, n, target);
+
+    if (index != -1) {
+        printf("Target found at index %d\n", index);
+    } else {
+        printf("Target not found\n");
+    }
+
+    return 0;
+}
